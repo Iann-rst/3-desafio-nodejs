73ce8164314d82d46bfe579cf1c3e4d965e167bb
Iann-rst <iannrst11@gmail.com>
2022-11-28 10:08:57 -0300

    feat: Deletar um repositório

--- src/index.js
+++ src/index.js
@@ -48,9 +48,9 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
